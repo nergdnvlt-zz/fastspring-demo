cdac971fedc43a21ea61c673ae701f775b528985
nergdnvlt <tylerlundgren@yahoo.com>
2021-01-11 13:32:26 -0700

    Building out purchase pages for account creation demos.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,9 @@
 class PagesController < ApplicationController
   def index; end
+
+  def free_trial
+  end
+
+  def freemium
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"  
+
+  get '/free-trial', to: 'pages#free_trial'
+  get '/freemium', to: 'pages#freemium'
 end
